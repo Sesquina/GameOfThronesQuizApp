be9d4053575a9cffd23581b51b5ae825a7921653
Samira Esquina <samiraesquina@gmail.com>
2019-07-02 14:57:31 -0400

    Uploaded updated

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 This is my Quiz App based on the HBO TV Series Game of Thrones
+I used javascript/jQuery/CSS/html
+
+/* User story:
+
+This quiz app allows users to take a quiz on a topic about Game of Thrones.
+The app starts with an introduction page and by clicking the start button, the user is redirected to the first set of multiple choice questions on the quiz.
+
+If the user gets the selected answer correct, an image will appear with the text: Correct! And a button that directs the user to the next question.
+
+If the user does not get the selected answer correct, the correct answer appears, and a button directs the user to the next question.
+Through each question, the user knows which question they are on and what their score is.
+
+Finally, once they are done all 10 questions, a button appears asking them to play again, and it redirects the user to the start of the quiz. */
---
+++ data.js
@@ -0,0 +1,109 @@
+const store = [
+    {
+      question: 'What’s the name of Arya Stark’s sword?',
+      answers: [
+        'Longclaw', // 0
+        'Oathkeeper', // 1
+        'Needle', // 2
+        'Ice', // 3
+      ],
+      correctAnswer: 'Needle'
+    },
+
+    {
+      question: 'What does the word Khaleesi refer to?',
+      answers: [
+        'One of Daenerys Targaryen’s captured cities',
+        'A title given to the wife of a Dothraki warlord',
+        'All Men Must Die',
+        'The name of the Night King',
+      ],
+      correctAnswer: 'A title given to the wife of a Dothraki warlord',
+    },
+
+    {
+      question: 'Who\'s family motto is "Winter is Coming"?',
+      answers: [
+        'House Stark',
+        'House  Lannister',
+        'House Tyrell',
+        'House Targaryen',
+      ],
+      correctAnswer: 'House Stark',
+    },
+
+    {
+      question: 'Which character besides Jon Snow has also died and been resurrected by a Red Priest?',
+      answers: [
+        'Gendry Waters',
+        'The Hound',
+        'Beric Dondarrion',
+        'Robb Stark',
+      ],
+      correctAnswer: 'Beric Dondarrion',
+    },
+
+    {
+      question: 'Arya’s direwolf is named…?',
+      answers: [
+        'Visenya',
+        'Nymeria',
+        'Aemon',
+        'Viserion',
+      ],
+      correctAnswer: 'Nymeria',
+    },
+
+    {
+      question: 'What is The Tyrell ancestral home?',
+      answers: [
+        'Winterfell',
+        'Highgarden',
+        'The Vale',
+        'Stormlands'
+      ],
+      correctAnswer: 'Highgarden',
+    },
+
+    {
+      question: 'What did Tyrion Lannister murder his father, Tywin, with?',
+      answers: [
+        'A dagger',
+        'A crossbow',
+        'Poisoned wine',
+        'He strangled him with a necklace'
+      ],
+      correctAnswer:'A crossbow',
+    },
+
+    {
+      question: 'Aside from Valyrian Steel, what else can kill White Walkers?',
+      answers: [
+        'A Raven',
+        'Greyscale',
+        'Ice',
+        'Dragon glass'
+      ],
+      correctAnswer:'Dragon glass',
+    },
+
+    {
+      question: 'Which king did Jamie Lannister slay to earn his “Kingslayer” nickname?',
+      answers: [
+        'King Robert Baratheon',
+        'King Aegon I Targaryen',
+        'King Aerys II Targaryen',
+        'King Joffrey Baratheon',
+      ],
+      correctAnswer: 'King Aerys II Targaryen',
+    },
+
+    {
+      question: 'Who are Jon Snow\'s Real Parents?',
+      answers: [
+        'Wylla and Eddard Stark',
+        'Elia Martell and Viserys Targaryen', 'Lyanna Stark and Rhaegar Targaryen', 'Cersie Lannister and Robert Baratheon'
+      ],
+      correctAnswer: 'Lyanna Stark and Rhaegar Targaryen',
+    }
+];
